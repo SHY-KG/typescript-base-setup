9943950ad15d262e97e5973e9619553ef56d4285
SHY-KG <tjdghks3078etc@gmail.com>
2022-01-18 21:48:31 +0900

    jest-test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-export function sum(a: number, b: number): number {
-  return a + b;
-}
+export const max = (numbers: number[]) => Math.max(...numbers);
+
+export const minimum = (numbers: number[]) => Math.min(...numbers);
+
+export const average = (numbers: number[]) =>
+  numbers.reduce(
+    (accumulator, currentValue, currentIndex, { length }) =>
+      accumulator + currentValue / length,
+    0
+  );
+
+export const sort = (numbers: number[]) => numbers.sort((a, b) => a - b);
+
+export const median = (numbers: number[]) => {
+  const { length } = numbers;
+  const middle = Math.floor(length / 2);
+  return length % 2
+    ? numbers[middle]
+    : (numbers[middle - 1] + numbers[middle]) / 2;
+};
--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -1,9 +1,33 @@
-import { sum } from '..';
+import { max, minimum, average, sort, median } from '..';
 
-describe('index test', () => {
-  it('calculates a + b', () => {
-    expect(sum(1, 2)).toBe(3);
-    expect(sum(3, 4)).toBe(7);
-    expect(sum(1, 1)).toBe(2);
+describe('index Test', () => {
+  // Maximum
+  it('Gets maximum value', () => {
+    expect(max([1, 2, 3, 4, 5, 6])).toBe(6);
+    expect(max([1, 3, 5, 7, 9, 11])).toBe(11);
+  });
+  // Minimum
+  it('Gets minimum value', () => {
+    expect(minimum([1, 2, 3, 4, 5, 6])).toBe(1);
+    expect(minimum([3, 5, 7, 9, 11])).toBe(3);
+  });
+  // Average
+  it('Gets average value', () => {
+    expect(average([1, 2, 3, 4, 5])).toBe(3);
+    expect(average([15, 25, 35, 45, 55])).toBe(35);
+  });
+  // Sorts
+  it('Sorts the array', () => {
+    expect(sort([6, 5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5, 6]);
+    expect(sort([22, 55, 11, 33, 12, 5])).toEqual([5, 11, 12, 22, 33, 55]);
+  });
+  //  Median
+  it('Gets the median for odd length', () => {
+    expect(median([1, 3, 5, 7, 9])).toBe(5);
+    expect(median([13, 15, 17, 19, 21])).toBe(17);
+  });
+  it('Gets the median for even length', () => {
+    expect(median([1, 2, 3, 4, 5, 6, 7, 8])).toBe(4.5);
+    expect(median([5, 10, 15, 20, 25, 30, 35, 40])).toBe(22.5);
   });
 });
